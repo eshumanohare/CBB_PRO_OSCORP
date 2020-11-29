3bffc56629d8a7645755f4b1026bf89c0e77ac3f
eshumanohare <37545945+eshumanohare@users.noreply.github.com>
2020-11-29 18:48:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ function mainApp() {
       const nextForm = parent.nextElementSibling;
 
       //  Check for validation
-      if (input.type === "number" && validateInput(input)) {
+      if (validateInput(input)) {
         // console.log("Okay");
         next_form(parent, nextForm);
-      } else if (input.value == "") {
+        // } else if(){
+      } else {
         console.log("in");
         parent.style.animation = "shake 0.5s ease";
       }
@@ -30,13 +31,50 @@ function next_form(parent, nextForm) {
 }
 
 function validateInput(user) {
+  // console.log(user.classList);
   if (user.value == "") {
     console.log("Number cannot be negative");
     error("rgb(189,87,87");
-  } else {
-    error("rgb(87,189,130)");
-    return true;
+    return false;
   }
+  if (
+    user.classList.contains("field-one") &&
+    (user.value < 6.981 || user.value > 28.11)
+  ) {
+    error("rgb(189,87,87)");
+    return false;
+  }
+  if (
+    user.classList.contains("field-two") &&
+    (user.value < 9.71 || user.value > 39.28)
+  ) {
+    console.log("in");
+    error("rgb(189,87,87");
+    return false;
+  }
+  if (
+    user.classList.contains("field-three") &&
+    (user.value < 43.79 || user.value > 188.5)
+  ) {
+    error("rgb(189,87,87");
+    return false;
+  }
+  if (
+    user.classList.contains("field-four") &&
+    (user.value < 143.5 || user.value > 2501.0)
+  ) {
+    error("rgb(189,87,87");
+    return false;
+  }
+  if (
+    user.classList.contains("field-five") &&
+    (user.value < 0.0 || user.value > 0.201)
+  ) {
+    error("rgb(189,87,87");
+    return false;
+  }
+  error("rgb(87,189,130)");
+  return true;
 }
 
 function error(color) {
