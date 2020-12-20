7443a91132329d638301a25cee59218b5cfd8a75
eshumanohare <37545945+eshumanohare@users.noreply.github.com>
2020-12-20 22:03:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function mainApp() {
   const arrows = document.querySelectorAll(".fa-arrow-right");
+  var inputsUser = new Array();
+  var counter = 0;
+  var json;
 
   arrows.forEach(arrow => {
     arrow.addEventListener("click", () => {
@@ -9,7 +12,13 @@ function mainApp() {
 
       //  Check for validation
       if (validateInput(input)) {
-        // console.log("Okay");
+        console.log("Okay");
+        inputsUser[counter++] = input.value;
+
+        if (nextForm.classList.contains("field-name6")) {
+          convertToJSON(inputsUser, nextForm);
+        }
+
         next_form(parent, nextForm);
         // } else if(){
       } else {
@@ -24,6 +33,40 @@ function mainApp() {
   });
 }
 
+function convertToJSON(inputsUser, nextForm) {
+  json = JSON.stringify(
+    `{"radius":${inputsUser[0]}, "texture":${inputsUser[1]}, "perimeter":${inputsUser[2]}, "area":${inputsUser[3]}, "concave":${inputsUser[4]}}`
+  );
+  console.log(json);
+  fetch("/hello", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: json
+  })
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(text) {
+      console.log("POST response : ");
+      console.log(text);
+      if (text == 1.0) {
+        console.log("abcd");
+        nextForm.innerHTML += `<h3 class="label-text result" style="left: 15%; top:-10px" >
+        Malignant Cancer
+        <span class="tooltip" style="top: -300px; left:60%;" >Malignant tumor, also known as cancerous cells, on the other hand, have the property of metastasis i.e. they spread rapidly to other parts of the body. They can occur anywhere in the body and can even be carried by the blood or lymphatic system. They often require intense treatment such as surgery, radiation, chemotherapy and immunotherapy. 
+They may recur after removal, sometimes in areas other the original site</span>
+      </h3>`;
+      } else if (text == 0.0) {
+        nextForm.innerHTML = `<h3 class="label-text result" style="left: 15%; top:-10px" >
+        Benign Cancer
+        <span class="tooltip" style="top: -300px; left:60%;" >Benign tumor also known as non cancerous cells don't spread to other areas of the body, a property known as metastasis. Thus, these tumors are less worrisome unless they pressing on nearby tissues, nerves, blood vessels or on the vital organs blocking channels. They can be removed by surgery and are unlikely to reoccur after removal.
+</span>
+      </h3>`;
+      }
+    });
+}
 function next_form(parent, nextForm) {
   parent.classList.add("innactive");
   parent.classList.remove("active");
@@ -33,7 +76,6 @@ function next_form(parent, nextForm) {
 function validateInput(user) {
   // console.log(user.classList);
   if (user.value == "") {
-    console.log("Number cannot be negative");
     error("rgb(189,87,87");
     return false;
   }
